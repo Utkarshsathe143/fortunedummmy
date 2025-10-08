13e9e93f9f85b37312a07370ed7038a1c2947257
Utkarshsathe143 <129713679+Utkarshsathe143@users.noreply.github.com>
2025-10-08 13:23:11 +0530

    first trigger

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express'); 
 var app = express();//Respond with "hello world" for requests that hit our root "/" 
 app.get('/', function (req, res) { 
- res.send('docker full prject check 3'); 
+ res.send('first trigger'); 
 });//listen to port 3000 by default 
 app.listen(process.env.PORT || 3000); 
 module.exports = app;
